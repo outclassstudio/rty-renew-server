3e6f3b788d87502a6ee4c393c82ce720b3433d19
outclassstudio <mlimshs@gmail.com>
2022-07-09 15:19:06 +0900

    Feat: Set controller base

--- src/gifts/gifts.controller.ts
+++ src/gifts/gifts.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('gifts')
-export class GiftsController {}
+export class GiftsController {
+  //내 선물 불러오기
+  @Get('/id')
+  getMyGift() {}
+
+  //선물 변경사항 저장(타입 및 위치)
+  @Patch('/id')
+  updateGift() {}
+
+  //특정 선물 삭제
+  @Delete('/id')
+  deleteGift() {}
+
+  //선불 보내기
+  @Post()
+  sendGift() {}
+
+  //내가 보낸 선물 조회
+  @Get('/send/id')
+  getSentGift() {}
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  //상점에서 아이템 불러오기
+  @Get()
+  getAllItems() {}
+
+  //내 아이템 불러오기
+  @Get('/:id')
+  getMyItems() {}
+
+  //선물 사기 및 포인트 차감
+  @Post('/:id')
+  buyItems() {}
+}
